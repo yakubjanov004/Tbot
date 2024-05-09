76bb9bb273fbf948ad855c35dcb78a7f7d995ca1
yakubjanov004 <ulugbekyoqubjonov59@gmail.com>
2024-05-09 19:20:11 +0500

    git ignored

---
+++ db.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+connection = sqlite3.connect('telegram_bot.db')
+
+cursor = connection.cursor()
+
+def main():
+    cursor.execute(
+    """CREATE TABLE IF NOT EXISTS foydalanuvchilar_soni(
+        username TEXT,
+        first_name TEXT,
+        last_name TEXT
+)
+"""
+)
+connection.commit()
+
+print(cursor.execute("SELECT * FROM foydalanuvchilar_soni").fetchall())
+
+def create_user(*args):
+    """
+    args - bu funksiya qabul qilayotgan foydalanuvchi ma'lumotlar to'plami
+    """
+    sql = """INSERT INTO foydalanuvchilar_soni(username,first_name,last_name)
+            VALUES(?,?,?)"""
+    cursor.execute(sql,args)
+    connection.commit()
+
+def info_users():
+    cursor.execute("SELECT username,first_name,last_name FROM foydalanuvchilar_soni")
+    return cursor.fetchall()
+   
+    
+
+if __name__ == "__main__":
+    main()
